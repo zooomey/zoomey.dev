8a9f2fc5b27df34ad1da4854b8d2b94c58c9722f
Claire Toomey <cmt392@drexel.edu>
2023-10-15 17:48:10 -0400

    zoomey.dev v2

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function myFunction() {
     alert("thank you 4 giving me one drexillion dollars :D");
   }
+
+function visitors(){
+  //
+  //https://sadgrl.online/learn/articles/last-update-visitors
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var site_data = JSON.parse(this.responseText);
+          var num_arr = site_data.info.views.toString().split("");
+          var num_str = "";
+          for (i = 0; i < num_arr.length; i++) {
+              num_str += num_arr[i];
+              if ( (num_arr.length-1 - i) % 3 == 0 && (num_arr.length-1 - i) != 0 ) {num_str += ",";}
+              var date_str = site_data.info.last_updated;
+              var date_obj = new Date(site_data.info.last_updated);
+              document.getElementById("lastupdate").innerHTML = (date_obj.getMonth()+1) + "-" + date_obj.getDate() + "-" + date_obj.getFullYear();
+          }
+          document.getElementById("hitcount").innerHTML = num_str;
+      }
+  };
+  xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=USERNAME", true);
+  xhttp.send();
+}
